8c06c1145644afafd698e9bbe9e1f370b5c61239
Fabo2303 <fabian666777@gmail.com>
2024-06-21 12:10:33 -0500

    add param

--- src/main/java/com/grupo5/sisvita/api/repositories/UbigeoRepository.java
+++ src/main/java/com/grupo5/sisvita/api/repositories/UbigeoRepository.java
@@ -3,16 +3,17 @@ package com.grupo5.sisvita.api.repositories;
 import com.grupo5.sisvita.api.entities.Ubigeo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface UbigeoRepository extends JpaRepository<Ubigeo, String> {
     @Query("SELECT DISTINCT u.provincia FROM Ubigeo u WHERE u.departamento = :departamento")
-    List<String> findProvinciasByDepartamento(String departamento);
+    List<String> findProvinciasByDepartamento(@Param("departamento") String departamento);
 
     @Query("SELECT DISTINCT u.distrito FROM Ubigeo u WHERE u.departamento = :departamento AND u.provincia = :provincia")
-    List<String> findDistritosByDepartamentoAndProvincia(String departamento, String provincia);
+    List<String> findDistritosByDepartamentoAndProvincia(@Param("departamento") String departamento, @Param("provincia") String provincia);
 
     @Query("SELECT u.ubigeo FROM Ubigeo u WHERE u.departamento = :departamento AND u.provincia = :provincia AND u.distrito = :distrito")
-    String findUbigeoByDepartamentoAndProvinciaAndDistrito(String departamento, String provincia, String distrito);
+    String findUbigeoByDepartamentoAndProvinciaAndDistrito(@Param("departamento") String departamento, @Param("provincia") String provincia,@Param("distrito") String distrito);
 }
